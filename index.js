d14f9160aac6f84d5bddd7eca42dbf87227c58bf
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-07-26 01:59:05 +0600

    add random pass generate

--- index.js
+++ index.js
@@ -21,4 +21,28 @@ function totalNum(data){
 }
 const numbers = [10, 20, 30, 8, -12, -10]
 const taskTwo = totalNum(numbers);
-console.log(taskTwo)
+// console.log(taskTwo)
+
+// Task : 6
+function randomPassword(data) {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()-_+=[]{}|:;"<>,.?/';
+  
+    const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+    let password = '';
+  
+    for (let i = 0; i < data; i++) {
+      const randomIndex = Math.floor(Math.random() * allChars.length);
+      password += allChars.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+  
+  // Example usage: Generate a random password of length 12
+  const passwordLength = 8;
+  const generatePass = randomPassword(passwordLength);
+//   console.log(generatePass);
+  
