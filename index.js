108b4150a00d02f2f40a478ee2851d97974b419e
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-07-27 14:02:10 +0600

    add five solving

--- index.js
+++ index.js
@@ -40,9 +40,66 @@ function randomPassword(data) {
   
     return password;
   }
-  
-  // Example usage: Generate a random password of length 12
   const passwordLength = 8;
   const generatePass = randomPassword(passwordLength);
 //   console.log(generatePass);
+  
+// Task 7 :
+function romanToInteger(data) {
+    const romanToInte = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+  
+    let result = 0;
+  
+    for (let i = 0; i < data.length; i++) {
+      const currentNumeral = romanToInte[data[i]];
+      const nextNumeralValue = romanToInte[data[i + 1]];
+  
+      if (nextNumeralValue > currentNumeral) {
+        result += nextNumeralValue - currentNumeral;
+        i++;
+      } else {
+        result += currentNumeral;
+      }
+    }
+  
+    return result;
+  }
+  
+  const romanInput = "III";
+  const result = romanToInteger(romanInput);
+//   console.log(result);
+  
+// Task 8
+function findSecondSmallest(data) {
+
+    let smallest = Infinity;
+    let secondSmallest = Infinity;
+  
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] < smallest) {
+        secondSmallest = smallest;
+        smallest = data[i];
+      } else if (data[i] < secondSmallest && data[i] !== smallest) {
+        secondSmallest = data[i];
+      }
+    }
+  
+    if (secondSmallest === Infinity) {
+      return "There is no second smallest number in the array.";
+    }
+  
+    return secondSmallest;
+  }
+  
+  const inputArray = [3, 1, 5, 4];
+  const taskEight = findSecondSmallest(inputArray);
+//   console.log(taskEight);
   
